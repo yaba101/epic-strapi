fd09e5b3fe4639a1bb4bc986d8039d07e6154aac
Yeabsira Mekuria <yabumek46@gmail.com>
2024-05-21 22:37:20 +0300

    feat: add basic sign-in and sign-up page UI

---
+++ frontend/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import { SigninForm } from "@/components/forms/sign-in-form";
+import React from "react";
+
+const SignInPage = () => {
+  return <SigninForm />;
+};
+
+export default SignInPage;
---
+++ frontend/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import { SignupForm } from "@/components/forms/sign-up-form";
+import React from "react";
+
+const SignUpPage = () => {
+  return <SignupForm />;
+};
+
+export default SignUpPage;
