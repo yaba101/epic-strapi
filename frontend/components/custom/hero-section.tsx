dcc0e77515a7c20147f2bbcf07ed0eb7abc91d61
Yeabsira Mekuria <yabumek46@gmail.com>
2024-05-21 23:36:56 +0300

    feat: update header and hero-section

--- frontend/components/custom/hero-section.tsx
+++ frontend/components/custom/hero-section.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
-import { StrapiImage } from "./strapi-image";
+import { getUserMeLoader } from "@/data/services/get-user-me-loader";
+import { StrapiImage } from "@/components/custom/strapi-image";
+
 interface ImageProps {
   id: number;
   url: string;
   alternativeText: string;
 }
+
 interface LinkProps {
   id: number;
   url: string;
   text: string;
 }
+
 interface HeroSectionProps {
   data: {
     id: number;
@@ -21,16 +25,21 @@ interface HeroSectionProps {
   };
 }
 
-export function HeroSection({ data }: Readonly<HeroSectionProps>) {
+export async function HeroSection({ data }: Readonly<HeroSectionProps>) {
+  const user = await getUserMeLoader();
   const { heading, subHeading, image, link } = data;
+
+  const userLoggedIn = user.ok;
+  const linkUrl = userLoggedIn ? "/dashboard" : link.url;
+
   return (
     <header className="relative h-[600px] overflow-hidden">
       <StrapiImage
-        src={image.url}
-        alt={image.alternativeText}
-        height={1080}
-        width={1920}
+        alt="Background"
         className="absolute inset-0 object-cover w-full h-full"
+        height={1080}
+        src={image.url}
+        width={1920}
       />
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white bg-black bg-opacity-20">
         <h1 className="text-4xl font-bold md:text-5xl lg:text-6xl">
@@ -39,9 +48,9 @@ export function HeroSection({ data }: Readonly<HeroSectionProps>) {
         <p className="mt-4 text-lg md:text-xl lg:text-2xl">{subHeading}</p>
         <Link
           className="mt-8 inline-flex items-center justify-center px-6 py-3 text-base font-medium text-black bg-white rounded-md shadow hover:bg-gray-100"
-          href={link.url}
+          href={linkUrl}
         >
-          {link.text}
+          {userLoggedIn ? "Dashboard" : link.text}
         </Link>
       </div>
     </header>
