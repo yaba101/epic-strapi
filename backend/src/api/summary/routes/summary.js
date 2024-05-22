724abc6862dd421e1210fd232cdf30dc9c87156e
Yeabsira Mekuria <yabumek46@gmail.com>
2024-05-22 12:18:25 +0300

    feat: user-specific data access

--- backend/src/api/summary/routes/summary.js
+++ backend/src/api/summary/routes/summary.js
@@ -1,15 +1,27 @@
-'use strict';
+"use strict";
 
 /**
  * summary router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
 module.exports = createCoreRouter("api::summary.summary", {
   config: {
     create: {
       middlewares: ["api::summary.on-summary-create"],
+    },
+    find: {
+      middlewares: ["global::is-owner"],
+    },
+    findOne: {
+      middlewares: ["global::is-owner"],
+    },
+    update: {
+      middlewares: ["global::is-owner"],
+    },
+    delete: {
+      middlewares: ["global::is-owner"],
     },
   },
 });
