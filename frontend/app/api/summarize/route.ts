b243e6dac3661591167e92d1fab03146af53240f
Yeabsira Mekuria <yabumek46@gmail.com>
2024-05-22 11:08:07 +0300

    feat: use langChain and Open AI to create a summary

--- frontend/app/api/summarize/route.ts
+++ frontend/app/api/summarize/route.ts
@@ -1,20 +1,12 @@
 import { NextRequest } from "next/server";
-import { fetchTranscript } from "@/lib/youtube-transcript";
 
+import { ChatOpenAI } from "@langchain/openai";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+
+import { fetchTranscript } from "@/lib/youtube-transcript";
 import { getUserMeLoader } from "@/data/services/get-user-me-loader";
 import { getAuthToken } from "@/data/services/get-token";
-
-async function getTranscript(id: string) {
-  try {
-    return await fetchTranscript(id);
-  } catch (error) {
-    if (error instanceof Error)
-      return new Response(JSON.stringify({ error: error.message }));
-    return new Response(
-      JSON.stringify({ error: "Failed to generate summary." })
-    );
-  }
-}
 
 function transformData(data: any[]) {
   let text = "";
@@ -29,7 +21,53 @@ function transformData(data: any[]) {
   };
 }
 
+const TEMPLATE = `
+INSTRUCTIONS: 
+  For the this {text} complete the following steps.
+  Generate the title based on the content provided
+  Summarize the following content and include 5 key topics, writing in first person using normal tone of voice.
+  
+  Write a youtube video description
+    - Include heading and sections.  
+    - Incorporate keywords and key takeaways
+
+  Generate bulleted list of key points and benefits
+
+  Return possible and best recommended key words
+`;
+
+async function generateSummary(content: string, template: string) {
+  const prompt = PromptTemplate.fromTemplate(template);
+
+  const model = new ChatOpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    modelName: process.env.OPENAI_MODEL ?? "gpt-3.5-turbo",
+    temperature: process.env.OPENAI_TEMPERATURE
+      ? parseFloat(process.env.OPENAI_TEMPERATURE)
+      : 0.7,
+    maxTokens: process.env.OPENAI_MAX_TOKENS
+      ? parseInt(process.env.OPENAI_MAX_TOKENS)
+      : 4000,
+  });
+
+  const outputParser = new StringOutputParser();
+  const chain = prompt.pipe(model).pipe(outputParser);
+
+  try {
+    const summary = await chain.invoke({ text: content });
+    return summary;
+  } catch (error) {
+    if (error instanceof Error)
+      return new Response(JSON.stringify({ error: error.message }));
+    return new Response(
+      JSON.stringify({ error: "Failed to generate summary." })
+    );
+  }
+}
+
 export async function POST(req: NextRequest) {
+  console.log("FROM OUR ROUTE HANDLER:", req.body);
+
   const user = await getUserMeLoader();
   const token = await getAuthToken();
 
@@ -48,31 +86,22 @@ export async function POST(req: NextRequest) {
       { status: 402 }
     );
 
-  console.log("FROM OUR ROUTE HANDLER:", req.body);
   const body = await req.json();
-  const videoId = body.videoId;
+  const { videoId } = body;
 
-  let transcript: Awaited<ReturnType<typeof getTranscript>>;
-
-  try {
-    transcript = await getTranscript(videoId);
-  } catch (error) {
-    console.error("Error processing request:", error);
-    if (error instanceof Error)
-      return new Response(JSON.stringify({ error: error.message }));
-    return new Response(JSON.stringify({ error: "Unknown error" }));
-  }
-
-  const transformedData = transformData(transcript as any);
-  console.log("Transcript:", transformedData);
+  let transcript: Awaited<ReturnType<typeof fetchTranscript>>;
 
   try {
-    return new Response(
-      JSON.stringify({ data: "return from our handler", error: null }),
-      {
-        status: 200,
-      }
-    );
+    transcript = await fetchTranscript(videoId);
+
+    const transformedData = transformData(transcript);
+    console.log("Transcript:", transformedData.text);
+
+    let summary: Awaited<ReturnType<typeof generateSummary>>;
+
+    summary = await generateSummary(transformedData.text, TEMPLATE);
+    console.log("Summary:", summary);
+    return new Response(JSON.stringify({ data: summary, error: null }));
   } catch (error) {
     console.error("Error processing request:", error);
     if (error instanceof Error)
--- frontend/components/forms/summary-form.tsx
+++ frontend/components/forms/summary-form.tsx
@@ -7,6 +7,7 @@ import { cn, extractYouTubeID } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
 import { SubmitButton } from "@/components/custom/submit-button";
 import { generateSummaryService } from "@/data/services/summary-service";
+import { createSummaryAction } from "@/actions/summary-action";
 
 interface StrapiErrorsProps {
   message: string | null;
@@ -56,6 +57,27 @@ export function SummaryForm() {
       setError({
         ...INITIAL_STATE,
         message: summaryResponseData.error,
+        name: "Summary Error",
+      });
+      setLoading(false);
+      return;
+    }
+
+    const payload = {
+      data: {
+        title: `Summary for video: ${processedVideoId}`,
+        videoId: processedVideoId,
+        summary: summaryResponseData.data,
+      },
+    };
+
+    try {
+      await createSummaryAction(payload);
+    } catch (error) {
+      toast.error("Error Creating Summary");
+      setError({
+        ...INITIAL_STATE,
+        message: "Error Creating Summary",
         name: "Summary Error",
       });
       setLoading(false);
