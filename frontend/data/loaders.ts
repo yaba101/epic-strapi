c06d2af9137cbc1eb221ae5dad97de2ee992a6ef
Yeabsira Mekuria <yabumek46@gmail.com>
2024-05-22 12:52:34 +0300

    feat: add search and pagination

--- frontend/data/loaders.ts
+++ frontend/data/loaders.ts
@@ -69,11 +69,26 @@ export async function getGlobalPageMetaData() {
   return await fetchData(url.href);
 }
 
-export async function getSummaries() {
-  const url = new URL(`api/summaries`, baseUrl);
-  return await fetchData(url.href);
-}
+export async function getSummaries(queryString: string, currentPage: number) {
+  const PAGE_SIZE = 4;
 
+  const query = qs.stringify({
+    sort: ["createdAt:desc"],
+    filters: {
+      $or: [
+        { title: { $containsi: queryString } },
+        { summary: { $containsi: queryString } },
+      ],
+    },
+    pagination: {
+      pageSize: PAGE_SIZE,
+      page: currentPage,
+    },
+  });
+  const url = new URL("/api/summaries", baseUrl);
+  url.search = query;
+  return fetchData(url.href);
+}
 export async function getSummaryById(summaryId: string) {
   return fetchData(`${baseUrl}/api/summaries/${summaryId}`);
 }
